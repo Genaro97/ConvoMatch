8e78e565f130920a8df921472e4cad88b55a588b
Genaro <galdana5189@gmail.com>
2019-10-01 19:45:35 -0500

    Primer commit

---
+++ main_app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from .models import Auto
+
+# Create your views here.
+def index(request):
+    autos = Auto.objects.all()
+    return render(request, 'index.html', {'autos' : autos})
